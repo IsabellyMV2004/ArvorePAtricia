9c225b664cdb3a71634dc10890a175661c372326
IsabellyMV2004 <isabellymarafaovoltareli2004@gmail.com>
2025-05-25 00:01:07 -0300

    criando classes No e ArvorePatricia

---
+++ src/ArvorePatricia.java
@@ -0,0 +1,19 @@
+public class ArvorePatricia {
+    No raiz;
+
+    public ArvorePatricia(){
+        raiz = null;
+    }
+
+
+    public void inserir(String info){
+        No novo;
+
+        if(raiz==null){
+            raiz.setCauda(new No(info,true,null,null));
+        }
+        else{
+
+        }
+    }
+}
---
+++ src/No.java
@@ -0,0 +1,51 @@
+public class No {
+    private String palavra;
+    private boolean flag;
+    private No cabeca;
+    private No cauda;
+
+    public No() {
+        cauda = cabeca = null;
+        flag = false;
+    }
+
+    public No(String palavra, boolean flag, No cabeca, No cauda){
+        this.palavra = palavra;
+        this.flag = flag;
+        this.cabeca = cabeca;
+        this.cauda = cauda;
+
+    }
+
+    public String getPalavra() {
+        return palavra;
+    }
+
+    public void setPalavra(String palavra) {
+        this.palavra = palavra;
+    }
+
+    public boolean isFlag() {
+        return flag;
+    }
+
+    public void setFlag(boolean flag) {
+        this.flag = flag;
+    }
+
+    public No getCabeca() {
+        return cabeca;
+    }
+
+    public void setCabeca(No cabeca) {
+        this.cabeca = cabeca;
+    }
+
+    public No getCauda() {
+        return cauda;
+    }
+
+    public void setCauda(No cauda) {
+        this.cauda = cauda;
+    }
+}
