a42640955a5eeda251a1a10b3c4f5ee4441faff1
IsabellyMV2004 <isabellymarafaovoltareli2004@gmail.com>
2025-05-31 16:52:51 -0300

    a

--- src/Fila.java
+++ src/Fila.java
@@ -1,3 +1,39 @@
 public class Fila {
+    private int fim;
+    No dados;
 
+    public void Fila(){
+        fim = 0;
+        dados = null;
+    }
+
+    public boolean FilaVazia(){
+        return fim == 0;
+    }
+
+    public  void Inserir(No elemento){
+        No aux = dados;
+        for (int i = 0; i < fim; i++) {
+            aux = aux.getCauda();
+        }
+        aux.setCauda(elemento);
+    }
+
+    public No Retirar(){
+        No aux = dados;
+        dados = dados.getCauda();
+        return aux;
+    }
+
+    public String ElementoInicio(){
+        return dados.getPalavra();
+    }
+
+    public String ElementoFim(){
+        No aux = dados;
+        for (int i = 0; i < fim; i++) {
+            aux = aux.getCauda();
+        }
+        return aux.getPalavra();
+    }
 }
